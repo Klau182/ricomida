90303f7f5c694845253f92fae49c4f69324b54b4
claudia <claudia_figueroai@hotmail.com>
2021-09-20 20:08:29 -0300

    desafio ricomida js

---
+++ assets/js/codigo.js
@@ -0,0 +1,22 @@
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+//ALERTA POR MEDIO DE CLICK A BOTON
+$( "#enviarCorreo" ).click(function() {
+    alert( "El correo fue enviado correctamente..." );
+  });
+
+//CAMBIO DE COLOR EN "INGREDIENTES" Y "PREPARACION" CON 2XCLICK
+$(".red").on("dblclick", function(){
+        $(this).css("color","red");
+    });
+
+//TOGGLE POR CLICK EN TITULO
+$( ".recetitulo" ).click(function() {
+    $( ".recetext" ).toggle(function() {
+    });
+  });
+
+  
